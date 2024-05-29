f917d0aa6ae2bb64e403f08f4a2d77a5d4db5407
tresenish <korolvolodymyr0@gmail.com>
2024-05-29 07:14:53 -0500

    1

--- src/SearchResult.js
+++ src/SearchResult.js
@@ -1,10 +1,12 @@
 import React, {useState} from 'react';
 import './SearchResult.css';
 function SearchResult(){
-    
+
     return (
         <div>
+            <div className='resultbox'>
 
+            </div>
         </div>
     );
 }
