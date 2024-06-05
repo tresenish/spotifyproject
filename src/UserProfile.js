39ac7ff7b1edc19eb601cf2372b60a54557d1ce7
Volodymyr Korol <157093366+tresenish@users.noreply.github.com>
2024-06-05 07:57:55 -0500

    adaptation for smartphone screen

--- src/UserProfile.js
+++ src/UserProfile.js
@@ -22,6 +22,13 @@ const UserProfile = ({ token }) => {
                 <div>
                     <h1 className='profile'> <span className='profile'>Welcome,</span> {profile.display_name}</h1>
                     <img className='profile' src={profile.images[0]?.url} alt="Profile" />
+                    <p className='profile'>
+                        1. Choose one of your playlists by clicking on its name.<br/>
+                        2. Search for a track by name using the search bar.<br/>
+                        3. Click on the <span className="profileAttach material-symbols-outlined">attach_file</span> icon to select the track.<br/>
+                        4. Add the selected track to the selected playlist by <br/> clicking the "Add to Playlist" button.<br/>
+                    </p>
+
                 </div>
                 ) : (<p>Loading...</p>)}
             </div>
